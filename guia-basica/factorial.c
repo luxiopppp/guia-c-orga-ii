1574c75aa5983bc7594b22c26d9d16c560145f13
Lucio Schraier <lucio.ese@gmail.com>
2025-08-31 01:13:45 -0300

    Ej 15

---
+++ guia-basica/factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int factorial(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+
+  return n*factorial(n-1);
+}
+
+
+
+int main()
+{
+  int n;
+  printf("Ingrese un numero: ");
+  scanf("%d", &n);
+   
+  printf("%d", factorial(n));
+
+  return 0;
+}
